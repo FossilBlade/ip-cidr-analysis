b6417c2f4fd2c08577cb02284c988feee41ee124
Raush <raush@work.com>
2020-01-15 00:33:25 +1300

    fix

--- cidr-manager-api/shell_utils.py
+++ cidr-manager-api/shell_utils.py
@@ -20,52 +20,26 @@ class cd:
 
 def run_cird_sh(cird_ip):
     sanitized_cird = cird_ip.replace('/', '-')
-    dok_cmd = f'docker run -d --rm --network=host -v {cird_script_folder}:/root/bin/NetDetails:rw -w /root/bin/NetDetails --name {sanitized_cird} cidr_runner:latest ./CIDRDetail.sh {cird_ip}'
-    # return subprocess.run([
-    #     f'docker run -d --rm --network=host -v {cird_script_folder}:/root/bin/NetDetails:rw -w /root/bin/NetDetails --name {sanitized_cird} cidr_runner:latest ./CIDRDetail.sh {cird_ip}'],
-    #     shell=True,
-    #     stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cird_script_folder).stdout.decode('utf-8')
-    os.system('whoami')
-    os.system('pwd')
-    os.system('echo $0')
-    return os.system(dok_cmd)
+    dok_comd = f'docker run -d --rm --network=host -v {cird_script_folder}:/root/bin/NetDetails:rw -w /root/bin/NetDetails --name {sanitized_cird} cidr_runner:latest ./CIDRDetail.sh {cird_ip}'
 
-    # with cd(cird_script_folder):
-    #     os.system(f"./CIDRDetail.sh {cird_ip} &")
-    #                          # stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    docker_name = subprocess.run([dok_comd],
+                                 shell=True,
+                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode('utf-8')
 
-    # return p
+    return docker_name
 
 
 def run_summary_sh(cird_ip):
-    # with cd(cird_script_folder):
     return subprocess.run([f"./summarize-results.sh {cird_ip.replace('/', '-')}"], shell=True,
                           stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cird_script_folder).stdout.decode('utf-8')
 
 
-# def check_cird_detail_sh_running(cird_ip):
-#     '''
-#     Check if there is any running process that contains the given name processName.
-#     '''
-#     # Iterate over the all the running process
-#     for proc in psutil.process_iter():
-#         try:
-#
-#             # Check if process name contains the given name string.
-#             cmdline_str = " ".join(proc.cmdline()).lower()
-#             if "CIDRDetail.sh".lower() in cmdline_str and cird_ip.lower() in cmdline_str:
-#                 return True
-#         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-#             pass
-#     return False;
-
-
 def check_cird_detail_sh_running(cird_ip):
     sanitized_cird = cird_ip.replace('/', '-')
-    docker_name = subprocess.call([
-        f'docker ps -qf "name={sanitized_cird}"'],
-        shell=True,
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode('utf-8')
+    dok_comd = f'docker ps -qf "name={sanitized_cird}"'
+    docker_name = subprocess.run([dok_comd],
+                                 shell=True,
+                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode('utf-8')
 
     if docker_name:
         return True
--- cidr-manager-api/test.py
+++ cidr-manager-api/test.py
@@ -3,7 +3,7 @@ import subprocess
 
 sanitized_cird = "mysql".replace('/', '-')
 docker_name = subprocess.run([
-                          f'docker ps -qf "name={sanitized_cird}"'],
+                          f'docker ps -aqf "name={sanitized_cird}"'],
                       shell=True,
                       stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode('utf-8')
 
